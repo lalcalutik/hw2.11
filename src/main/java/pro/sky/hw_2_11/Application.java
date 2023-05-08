0afbd07f240f755da8f57b98fa4b849b31a4f5d6
РОМАН <themorozushka@gmail.com>
2023-05-08 08:22:29 +0300

    hw is done

--- src/main/java/pro/sky/hw_2_11/Application.java
+++ src/main/java/pro/sky/hw_2_11/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Hw211Application {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Hw211Application.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
---
+++ src/main/java/pro/sky/hw_2_11/component/Basket.java
@@ -0,0 +1,23 @@
+package pro.sky.hw_2_11.component;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+@Component
+@SessionScope
+public class Basket {
+
+  private final Set<Integer> ids = new HashSet<>();
+
+  public void add(Set<Integer> ids) {
+    this.ids.addAll(ids);
+  }
+
+  public Set<Integer> get() {
+    return Collections.unmodifiableSet(ids);
+  }
+
+}
