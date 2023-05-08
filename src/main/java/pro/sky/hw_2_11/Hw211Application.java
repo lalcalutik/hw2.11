b0f5c32aba86eff73892f3b0c4ebfd44740157dc
РОМАН <themorozushka@gmail.com>
2023-05-08 07:49:37 +0300

    initial commit

---
+++ src/main/java/pro/sky/hw_2_11/Hw211Application.java
@@ -0,0 +1,13 @@
+package pro.sky.hw_2_11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw211Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw211Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/hw_2_11/Hw211ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.hw_2_11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw211ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
